53f72ab91f340cd5bbec3ceb41153ca786fb6d7e
chionz <chideraprince9@gmail.com>
2025-05-08 12:07:19 +0100

    **

--- api/v1/models/__init__.py
+++ api/v1/models/__init__.py
@@ -1,6 +1,4 @@
-from api.v1.models.activity_logs import ActivityLog
-from api.v1.models.waitlist import Waitlist
 from api.v1.models.user import User
 from api.v1.models.token_login import TokenLogin
 from api.v1.models.oauth import OAuth
-from api.v1.models.advertisement import Advert
+
