0295e566d706644f57a3a0ad23c4c954a2877618
chionz <chideraprince9@gmail.com>
2025-05-14 16:02:00 +0100

    dashboard individual route

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from api.v1.routes.auth import auth
 from api.v1.routes.user import user_router
 from api.v1.routes.eye_tests import eye_routes
+from api.v1.routes.dashboard import dash_routes
 from api.v1.routes.request_password import pwd_reset
 
 api_version_one = APIRouter(prefix="/api/v1")
@@ -11,3 +12,4 @@ api_version_one.include_router(auth)
 api_version_one.include_router(pwd_reset)
 api_version_one.include_router(user_router)
 api_version_one.include_router(eye_routes)
+api_version_one.include_router(dash_routes)
--- api/v1/routes/user.py
+++ api/v1/routes/user.py
@@ -15,51 +15,7 @@ from api.db.database import get_db
 from api.v1.services.user import user_service
 from api.v1.services.eye_tests import eyetest_service
 
-
-def dashboard_items(request: Request, db: Session = Depends(get_db)):
-    user_id = user_service.fetch_user_refresh_token(request, db=db)
-
-    if user_id == None:
-        return None
-    user = user_service.get_user_by_id(db=db, id=user_id)
-    return {"user": user, "user_id": user_id}
-
-
 user_router = APIRouter(prefix="/users", tags=["Users"])
-
-
-# User Dashboard View
-@user_router.post("/dashboard")
-async def dashboard(refresh_token:str, request: Request, db: Session = Depends(get_db)):
-    if refresh_token == None:
-        raise HTTPException(status_code=401, detail="no refresh token provided")
-    
-    token_data = user_service.verify_refresh_token(refresh_token=refresh_token)
-    user_id =token_data.id
-    user = user_service.get_user_by_id(db=db, id=user_id)
-    no_test_done = eyetest_service.all_test_count(db, user_id=user_id)
-    vision = eyetest_service.dashboard_vision_test(db, user_id=user_id)
-    # print(wallet, user)
-
-    user = jsonable_encoder(
-            user,
-            exclude=[
-                "password",
-                "is_super_admin",
-                "is_deleted",
-                "is_verified",
-                "updated_at",
-            ],
-    )
-    return success_response(
-        status_code=200,
-        message="User details retrieved successfully",
-        data= {
-            "user": user,
-            "test_taken": no_test_done,
-            "eye_test_data": vision
-        }
-    )
 
 
 """ 
