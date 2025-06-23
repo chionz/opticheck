1ab8dd71bf9a95960a8a7eac9014e6e49f8ae3d6
chionz <chideraprince9@gmail.com>
2025-06-23 01:01:13 +0100

    cors

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
