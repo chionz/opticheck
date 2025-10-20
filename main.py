7d5bf3454f6a25547f67e884ddbec5c9cbe01397
chionz <chideraprince9@gmail.com>
2025-10-20 03:50:03 +0100

    main

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ origins = [
 app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
